f1ff45fa8174af9e5332c6ed7a45349a04f5c86f
Xolreyli <mwball1986@gmail.com>
2025-05-13 13:51:06 +0100

    Modified Paddle.java

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,17 +1,46 @@
+import java.awt.*;
 import java.util.Set;
+
+import static utils.Constants.*;
 
 public class Paddle {
     private final int upKey;
     private final int downKey;
+    private double dy; // Vertical speed for the paddle
+    private Rectangle pos; // Paddle position
 
-    public Paddle(int upKey, int downKey) {
+    public Paddle(int upKey, int downKey, int x) {
         this.upKey = upKey;
         this.downKey = downKey;
+        this.pos = new Rectangle(x, BOARD_HEIGHT / 2 - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT);
     }
 
     public void handleActiveKeys(Set<Integer> activeKeys) {
-        if (activeKeys.contains(upKey)) {
+        dy = 0;
 
+        // Handle movement based on key input
+        if (activeKeys.contains(upKey)) {
+            dy -= PADDLE_SPEED;
+        }
+        if (activeKeys.contains(downKey)) {
+            dy += PADDLE_SPEED;
         }
     }
+
+    public void tick() {
+        pos.y += (int)dy;
+
+        // Prevent the paddle from moving out of bounds
+        pos.y = Math.clamp(pos.y, 0, BOARD_HEIGHT - PADDLE_HEIGHT);
+    }
+
+    public Rectangle getBounds() {
+        return pos;
+    }
+
+    public boolean checkBallCollision(Ball ball) {
+        // Simple paddle-ball collision logic (checking for intersection)
+        return ball.getBounds().intersects(this.getBounds());
+    }
 }
+
