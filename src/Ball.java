1b5ac8464383d6e6d6e6e4f91ec4316945ad4d5e
Xolreyli <mwball1986@gmail.com>
2025-05-20 11:08:07 +0100

    Update Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -1,39 +1,56 @@
-import static utils.Constants.*;
-
 import java.awt.*;
 import java.util.Random;
+import javax.swing.ImageIcon;
 
-public class Ball extends Sprite {
-    private double vx;
-    private double vy;
+public class Ball {
+    public static final int SIZE = 20;
+    private int x, y, xSpeed, ySpeed;
+    private Image ballImage;
 
     public Ball() {
-        super(BALL_IMAGE_PATH, 0, 0, BALL_SIZE, BALL_SIZE);
         reset();
+        ballImage = new ImageIcon("ball.png").getImage();  // Load the ball image
     }
 
     public void reset() {
-        // Reset the ball's position to the center
-        pos.setLocation(BOARD_WIDTH / 2 - BALL_SIZE / 2, BOARD_HEIGHT / 2 - BALL_SIZE / 2);
-
-        // Assign random direction to the ball (up-left, up-right, down-left, down-right)
+        x = 390;
+        y = 290;
         Random rand = new Random();
-        vx = rand.nextBoolean() ? 1 : -1; // Random horizontal direction (1 or -1)
-        vy = rand.nextBoolean() ? 1 : -1; // Random vertical direction (1 or -1)
-
-        // Set random speed
-        vx *= BALL_SPEED;
-        vy *= BALL_SPEED;
+        xSpeed = rand.nextInt(2) == 0 ? 4 : -4;
+        ySpeed = rand.nextInt(2) == 0 ? 4 : -4;
     }
 
-    @Override
-    public void tick() {
-        // Update the ball's position based on velocity
-        pos.translate((int)vx, (int)vy);
+    public void update() {
+        x += xSpeed;
+        y += ySpeed;
+    }
 
-        // Ball collision with top/bottom walls
-        if (pos.y <= 0 || pos.y + size.height >= BOARD_HEIGHT) {
-            vy *= -1; // Reverse vertical direction
-        }
+    public void draw(Graphics g) {
+        g.drawImage(ballImage, x, y, SIZE, SIZE, null);  // Draw the ball image
+    }
+
+    public void reverseVerticalDirection() {
+        ySpeed = -ySpeed;
+    }
+
+    public void reverseHorizontalDirection() {
+        xSpeed = -xSpeed;
+    }
+
+    public void increaseSpeed() {
+        if (xSpeed > 0) xSpeed++;
+        else xSpeed--;
+    }
+
+    public boolean collidesWith(Paddle paddle) {
+        return x + SIZE >= paddle.getX() && x <= paddle.getX() + Paddle.WIDTH && y + SIZE >= paddle.getY() && y <= paddle.getY() + Paddle.HEIGHT;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
