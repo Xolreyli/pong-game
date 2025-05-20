3806b35acff83f65a9ff96e07b93a2801b06011a
Xolreyli <mwball1986@gmail.com>
2025-05-20 11:12:44 +0100

    Create Scoreboard.java

---
+++ src/Scoreboard.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class Scoreboard {
+    private Player player1, player2;
+
+    public Scoreboard(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.PLAIN, 24));
+        g.drawString("Player 1: " + player1.getScore(), 20, 30);
+        g.drawString("Player 2: " + player2.getScore(), 600, 30);
+    }
+}
