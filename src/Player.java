7f787f439e826b2e14c0a153af7d3d478c1bac90
Xolreyli <mwball1986@gmail.com>
2025-05-20 11:10:20 +0100

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -2,59 +2,43 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 
-public class Paddle {
-    public static final int WIDTH = 20, HEIGHT = 100;
-    private int x, y;
+public class Player {
+    private int score;
+    private Paddle paddle;
     private int upKey, downKey;
-    private boolean movingUp, movingDown;
-    private Image paddleImage;
 
-    public Paddle(int x, int y, int upKey, int downKey) {
-        this.x = x;
-        this.y = y;
+    public Player(int x, int paddleY, int upKey, int downKey) {
+        this.score = 0;
+        this.paddle = new Paddle(x, paddleY, upKey, downKey);
         this.upKey = upKey;
         this.downKey = downKey;
-        paddleImage = new ImageIcon("paddle.png").getImage();  // Load the paddle image
     }
 
     public void update() {
-        if (movingUp && y > 0) {
-            y -= 5;
-        }
-        if (movingDown && y < 600 - HEIGHT) {
-            y += 5;
-        }
+        paddle.update();
     }
 
     public void draw(Graphics g) {
-        g.drawImage(paddleImage, x, y, WIDTH, HEIGHT, null);  // Draw the paddle image
+        paddle.draw(g);
+    }
+
+    public void incrementScore() {
+        score++;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Paddle getPaddle() {
+        return paddle;
     }
 
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == upKey) {
-            movingUp = true;
-        } else if (e.getKeyCode() == downKey) {
-            movingDown = true;
-        }
+        paddle.keyPressed(e);
     }
 
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == upKey) {
-            movingUp = false;
-        } else if (e.getKeyCode() == downKey) {
-            movingDown = false;
-        }
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void reset() {
-        y = 250; // Reset paddles to center position
+        paddle.keyReleased(e);
     }
 }
