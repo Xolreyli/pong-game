93863297cba50361f250005ccf21fcf3457cea23
Xolreyli <mwball1986@gmail.com>
2025-05-20 11:13:55 +0100

    Update Constants.java

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,37 +1,36 @@
-package utils;
+public class Constants {
 
-public final class Constants {
-    private Constants() {
-        // prevents instantiation
-    }
+    // Screen Dimensions
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
 
-    // Board constants
-    public static final int BOARD_WIDTH = 640;
-    public static final int BOARD_HEIGHT = 480;
-    public static final int TICK_DELAY = 25; // 45 FPS
-
-    // Player constants
-    public static final String PLAYER_IMAGE_PATH = "resources/amogus.png";
-    public static final int PLAYER_WIDTH = 60;
-    public static final int PLAYER_HEIGHT = 80;
-    public static final int PLAYER_SPEED = 10;
-
-    // Wall constants
-    public static final String WALL_IMAGE_PATH = "resources/wall.png";
-    public static final int WALL_WIDTH = 150;
-    public static final int WALL_HEIGHT = 150;
-
-    // Ball constants
-    public static final String BALL_IMAGE_PATH = "resources/ball.png";
+    // Ball Constants
     public static final int BALL_SIZE = 20;
-    public static final double BALL_SPEED = 5.0; // Adjust ball speed
+    public static final int BALL_INITIAL_SPEED_X = 4;
+    public static final int BALL_INITIAL_SPEED_Y = 4;
 
-    // Paddle constants
-    public static final String PADDLE_IMAGE_PATH = "resources/paddle.png";
-    public static final int PADDLE_WIDTH = 15;
-    public static final int PADDLE_HEIGHT = 80;
-    public static final int PADDLE_SPEED = 8; // Adjust paddle speed
+    // Paddle Constants
+    public static final int PADDLE_WIDTH = 20;
+    public static final int PADDLE_HEIGHT = 100;
+    public static final int PADDLE_SPEED = 5;
 
-    // Rally constants
-    public static final int MAX_SCORE = 11; // Winning score for the match
+    // Wall Constants
+    public static final int WALL_HEIGHT = 20; // For both top and bottom walls
+    public static final int WALL_WIDTH = SCREEN_WIDTH;  // Full screen width for top and bottom walls
+
+    // Score Constants
+    public static final int WINNING_SCORE = 11;
+
+    // Rally Counter Constants
+    public static final int MAX_RALLY_DISPLAY = 1000; // Optional max rally display if you choose to store it
+
+    // FPS (frames per second) Constants
+    public static final int FPS = 60;  // Game updates at ~60 FPS
+
+    // Paddle Movement Keys
+    public static final int PLAYER_1_UP_KEY = 'w';
+    public static final int PLAYER_1_DOWN_KEY = 's';
+    public static final int PLAYER_2_UP_KEY = KeyEvent.VK_UP;
+    public static final int PLAYER_2_DOWN_KEY = KeyEvent.VK_DOWN;
+
 }
